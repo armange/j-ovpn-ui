a63a361210fedeb7ba2c17987c653d11a27e7949
Diego Armange Costa <diego.armange@gpabr.com>
2022-07-13 11:26:16 -0300

    (No-Issue) Ovpn CLI-Adapter.

---
+++ cli-adapter/src/main/java/br/com/armange/openvpn/App.java
@@ -0,0 +1,13 @@
+package br.com.armange.openvpn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
